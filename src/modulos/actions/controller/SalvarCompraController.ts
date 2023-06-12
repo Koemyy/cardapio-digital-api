5b24ee1a142eadc81b73d274505c047c9419638e
Ueverson Ramos <ueverson.ramos@liax.tech>
2023-06-12 17:51:53 -0300

    refactor do endpoint de salvar Compra no banco

--- src/modulos/actions/controller/SalvarCompraController.ts
+++ src/modulos/actions/controller/SalvarCompraController.ts
@@ -4,13 +4,14 @@ import { SalvarCompraUseCase } from "../useCase/SalvarCompraUseCase";
 export class SalvarCompraController {
     async handle(request: Request, response: Response) {
 
-        let {ped_id, cli_id, pro_id, ped_status} = request.body;
+        let { cli_id, pro_id, ped_status, ped_quantidade} = request.body;
 
         const ped_horario = new Date();
     
         const salvarCompraUseCase = new SalvarCompraUseCase();
         
-        const result = await salvarCompraUseCase.execute({cli_id, pro_id, ped_status, ped_horario})
+        console.log('chegou', cli_id, pro_id, ped_status, ped_quantidade)
+        const result = await salvarCompraUseCase.execute({cli_id, pro_id, ped_status, ped_horario, ped_quantidade})
 
         return response.json({result: result});
     }
